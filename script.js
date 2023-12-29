d99e20ba595be91786d48e5a8d15a828212ec779
tnknguyen_ <106684954+minionly@users.noreply.github.com>
2023-12-29 13:30:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for the toggle button
+const toggleButton = document.getElementById('toggleButton');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('effects-enabled');
+});
